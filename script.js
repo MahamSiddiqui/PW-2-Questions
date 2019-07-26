04d2933038adfaefebed75a9afcdb75de2d95e24
Maham Siddiqui <49103875+MahamSiddiqui@users.noreply.github.com>
2019-07-26 17:31:54 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -42,13 +42,66 @@ let videos = [
     {
         title: "Rosy Ka Kitchen",
         lengthInMinutes: 3,
-        category: "Reciepy",
+        category: "Recipe",
         uploadDate: new Date("05-22-2019"),
         tags: ", salaad, raita, biryani",
         features: ["Live", "360°", "HDR"],
         viewCount: 4500,
         rating: 4.6
     },
+    {
+        title: "How to use DOCKER- tutorial by PIAIC",
+        lengthInMinutes: 120,
+        category: "Education",
+        uploadDate: new Date("07-26-2018"),
+        tags: "version/variant, Git",
+        features: ["Purchased", "HD"],
+        viewCount: 4000,
+        rating: 4.1
+    },
+    {
+        title: "Pakistan Star",
+        lengthInMinutes: 60,
+        category: "Entertainment",
+        uploadDate: new Date("07-26-2019"),
+        tags: "comady, funny, dance, drama play",
+        features: ["Subtitles/CC", "3D", "HD"],
+        viewCount: 3500,
+        rating: 4.0
+    },
+    {
+        title: "5 Minutes Recipe",
+        lengthInMinutes: 5,
+        category: "Recipe",
+        uploadDate: new Date("07-23-2019"),
+        tags: "cheesy pasta",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 3500,
+        rating: 4.4
+    },
+    {
+        title: "Her Lamha Purjoshh",
+        lengthInMinutes: 30,
+        category: "Entertainment",
+        uploadDate: new Date("06-26-2018"),
+        tags: "comady, dance, drama play",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 4003,
+        rating: 3.1
+    },
+    {
+        title: " Episode # 01 - Nukta",
+        lengthInMinutes: 80,
+        category: "Education",
+        uploadDate: new Date("07-26-2019"),
+        tags: "Gerenal Knowledge",
+        features: ["Subtitles/CC", "3D", "HD"],
+        viewCount: 4000,
+        rating: 4.2
+    },
+
+
+
 ];
 
 //Q1
@@ -76,9 +129,53 @@ let videos = [
 //     }
 // }
 
-//Q4
-for (let i = 0; i < videos.length; i++) {
-    if (videos[i].lengthInMinutes.) {
+//Q4 //incomplete hai ye question........
+/*for (let i = 0; i < videos.length; i++) {
+    if (videos[i].lengthInMinutes) {
         console.log("Title: " + videos[i].title);
     }
-}
+}*/
+
+//Q5 check
+/* for (let i = 0; i < videos.length; i++) {
+     if (videos[i].lengthInMinutes >= 30) {
+        console.log("Title: " + videos[i].title);
+     }
+}*/
+
+//Q6
+/*for (let i = 0; i < videos.length; i++) {
+    if (videos[i].category === "Education") {
+        console.log("Title: " + videos[i].title);
+    }
+} */
+
+//Q7 incomplete hai.....
+// for (let i = 0; i < videos.length; i++) {
+//     if (videos[i].tags == "javascript") {
+//         console.log("Title: " + videos[i].title);
+//     }
+// }
+
+//Q8 incomplete............
+// for (let i = 0; i < videos.length; i++) {
+//     if (videos[i].features) {
+//         console.log("Title: " + videos[i].title);
+//     }
+// }
+
+//Q9 incomplete.....
+// for (let i = 0; i < videos.length; i++) {
+//     if (videos[i].uploadDate {
+//         console.log("Title: " + videos[i].title);
+//     }
+// }
+
+//Q10incomplete.....
+// for (let i = 0; i < videos.length; i++) {
+//     if (videos[i].uploadDate. {
+//         console.log("Title: " + videos[i].title);
+//     }
+// }
+
+//Q11
