e418764260a2911794530cb5bbecc56952d8927e
Maham Siddiqui <49103875+MahamSiddiqui@users.noreply.github.com>
2019-07-26 14:19:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+let videos = [
+    {
+        title: "Photoshop tutorial",
+        lengthInMinutes: 70,
+        category: "Education",
+        uploadDate: new Date("07-22-2019"),
+        tags: "design, digital, photoshop, creativity",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 4700,
+        rating: 4.3
+    },
+    {
+        title: "Episode # 01 - The Best Comedy Show",
+        lengthInMinutes: 19,
+        category: "Entertainment",
+        uploadDate: new Date("07-03-2019"),
+        tags: "comedy, funny",
+        features: ["Subtitles/CC", "3D", "HD"],
+        viewCount: 145615,
+        rating: 3.9
+    },
+    {
+        title: "How to use FOR EACH loop - tutorial by Tech Karo",
+        lengthInMinutes: 6,
+        category: "Education",
+        uploadDate: new Date("11-10-2018"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 9004,
+        rating: 4.5
+    },
+    {
+        title: "Easy tutorial",
+        lengthInMinutes: 30,
+        category: "Education",
+        uploadDate: new Date("03-12-2019"),
+        tags: "grid, flex",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 5000,
+        rating: 4.1
+    },
+    {
+        title: "Rosy Ka Kitchen",
+        lengthInMinutes: 3,
+        category: "Reciepy",
+        uploadDate: new Date("05-22-2019"),
+        tags: ", salaad, raita, biryani",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 4500,
+        rating: 4.6
+    },
+];
+
+//Q1
+// for (let i = 0; i < videos.length; i++) {
+//     console.log("Title: " + videos[i].title);
+//     console.log("Length: " + videos[i].lengthInMinutes + " minutes");
+//     console.log("Category: " + videos[i].category);
+//     console.log("Views: " + videos[i].viewCount);
+//     console.log("Uploaded On: " + new Date());
+//     console.log("Rating: " + videos[i].rating);
+//     console.log("");
+// }
+
+//Q2
+// for (let i = 0; i < videos.length; i++) {
+//     if (videos[i].lengthInMinutes <= 3) {
+//         console.log("Title: " + videos[i].title);
+//     }
+// }
+
+//Q3
+// for (let i = 0; i < videos.length; i++) {
+//     if (videos[i].lengthInMinutes >= 20) {
+//         console.log("Title: " + videos[i].title);
+//     }
+// }
+
+//Q4
+for (let i = 0; i < videos.length; i++) {
+    if (videos[i].lengthInMinutes.) {
+        console.log("Title: " + videos[i].title);
+    }
+}
